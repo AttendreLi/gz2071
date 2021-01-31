8bb739a825d84dff9af69f533b4abe2ad6207676
Attendre <1825887436@qq.com>
2021-01-31 16:43:19 +0800

    张三第二次用idea提交

--- git的基本使用/src/com.gec.demotest/DemoTest1.java
+++ git的基本使用/src/com.gec.demotest/DemoTest1.java
@@ -4,6 +4,9 @@ public class DemoTest1 {
     void  aa(){
         System.out.println("aa");
     }
+    void aa_2(){
+        System.out.println("aa_2");
+    }
     public static void main(String[] args) {
 
     }
